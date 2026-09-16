4c5ed6f9d0f9a3e1d84fc661371bed9338dae1be
dongengkids-wq <dongengkids@gmail.com>
2026-09-16 17:52:24 +0700

    Tambah fitur scan ISBN di halaman penerbit & ubah urutan detail Pembagian Buku Master jadi per provinsi

--- app.py
+++ app.py
@@ -3506,7 +3506,7 @@ def pembagian_buku_detail():
     page = min(page, total_halaman)
     offset = (page - 1) * per_page
 
-    query_paged = query + " ORDER BY warna_area NULLS LAST, nama_perpustakaan ASC LIMIT %s OFFSET %s"
+    query_paged = query + " ORDER BY provinsi ASC NULLS LAST, kabupaten_kota ASC, nama_perpustakaan ASC LIMIT %s OFFSET %s"
     cur.execute(query_paged, tuple(params + [per_page, offset]))
     daftar_penyebaran = cur.fetchall()
 
